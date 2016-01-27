9cd371e9056ae945f7799c84c4aa8cfdc2216132
Kemel Zaidan <kemelzaidan@gmail.com>
2016-01-26 23:03:07 -0200

    adds gravatar support

--- minstant.js
+++ minstant.js
@@ -1,6 +1,11 @@
 Chats = new Mongo.Collection("chats");
 
 if (Meteor.isClient) {
+  // Ask for username on sign up
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+  });
+
   // set up the main template the the router will use to build pages
   Router.configure({
     layoutTemplate: 'ApplicationLayout'
@@ -9,37 +14,37 @@ if (Meteor.isClient) {
   Router.route('/', function () {
     console.log("rendering root /");
     this.render("navbar", {to:"header"});
-    this.render("lobby_page", {to:"main"});  
+    this.render("lobby_page", {to:"main"});
   });
 
   // specify a route that allows the current user to chat to another users
   Router.route('/chat/:_id', function () {
-    // the user they want to chat to has id equal to 
-    // the id sent in after /chat/... 
+    // the user they want to chat to has id equal to
+    // the id sent in after /chat/...
     var otherUserId = this.params._id;
     // find a chat that has two users that match current user id
     // and the requested user id
     var filter = {$or:[
-                {user1Id:Meteor.userId(), user2Id:otherUserId}, 
+                {user1Id:Meteor.userId(), user2Id:otherUserId},
                 {user2Id:Meteor.userId(), user1Id:otherUserId}
                 ]};
     var chat = Chats.findOne(filter);
     if (!chat){// no chat matching the filter - need to insert a new one
       chatId = Chats.insert({user1Id:Meteor.userId(), user2Id:otherUserId});
     }
-    else {// there is a chat going already - use that. 
+    else {// there is a chat going already - use that.
       chatId = chat._id;
     }
     if (chatId){// looking good, save the id to the session
       Session.set("chatId",chatId);
     }
     this.render("navbar", {to:"header"});
-    this.render("chat_page", {to:"main"});  
+    this.render("chat_page", {to:"main"});
   });
 
   ///
-  // helper functions 
-  /// 
+  // helper functions
+  ///
   Template.available_user_list.helpers({
     users:function(){
       return Meteor.users.find();
@@ -48,8 +53,8 @@ if (Meteor.isClient) {
  Template.available_user.helpers({
     getUsername:function(userId){
       user = Meteor.users.findOne({_id:userId});
-      return user.profile.username;
-    }, 
+      return user.username;
+    },
     isMyUser:function(userId){
       if (userId == Meteor.userId()){
         return true;
@@ -65,10 +70,10 @@ if (Meteor.isClient) {
     messages:function(){
       var chat = Chats.findOne({_id:Session.get("chatId")});
       return chat.messages;
-    }, 
+    },
     other_user:function(){
       return ""
-    }, 
+    },
 
   })
  Template.chat_page.events({
@@ -85,7 +90,7 @@ if (Meteor.isClient) {
         msgs = [];
       }
       // is a good idea to insert data straight from the form
-      // (i.e. the user) into the database?? certainly not. 
+      // (i.e. the user) into the database?? certainly not.
       // push adds the message to the end of the array
       msgs.push({text: event.target.chat.value});
       // reset the form
@@ -102,18 +107,26 @@ if (Meteor.isClient) {
 
 // start up script that creates some users for testing
 // users have the username 'user1@test.com' .. 'user8@test.com'
-// and the password test123 
+// and the password test123
 
 if (Meteor.isServer) {
+
+  Accounts.onCreateUser(function(options, user) {
+    console.log("Creating user "+user.username);
+    user.profile = {};
+    user.profile["avatar"] = Gravatar.imageUrl(user.emails[0].address);
+    return user;
+  });
+
   Meteor.startup(function () {
     if (!Meteor.users.findOne()){
       for (var i=1;i<9;i++){
         var email = "user"+i+"@test.com";
         var username = "user"+i;
-        var avatar = "ava"+i+".png"
+        var avatar = "/ava"+i+".png"
         console.log("creating a user with password 'test123' and username/ email: "+email);
-        Meteor.users.insert({profile:{username:username, avatar:avatar}, emails:[{address:email}],services:{ password:{"bcrypt" : "$2a$10$I3erQ084OiyILTv8ybtQ4ON6wusgPbMZ6.P33zzSDei.BbDL.Q4EO"}}});
+        Meteor.users.insert({username:username, profile:{avatar:avatar}, emails:[{address:email}],services:{ password:{"bcrypt" : "$2a$10$I3erQ084OiyILTv8ybtQ4ON6wusgPbMZ6.P33zzSDei.BbDL.Q4EO"}}});
       }
-    } 
-  });
+    }
+  }); // end of Meteor.startup
 }
